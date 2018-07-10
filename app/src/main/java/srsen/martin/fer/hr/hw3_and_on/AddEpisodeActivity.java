7a9482a9af44523d5da66af2228e545b026fe151
Martin Sršen <martin.srsen@fer.hr>
2018-07-10 20:02:17 +0200

    Added layouts for each element

---
+++ app/src/main/java/srsen/martin/fer/hr/hw3_and_on/AddEpisodeActivity.java
@@ -0,0 +1,14 @@
+package srsen.martin.fer.hr.hw3_and_on;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddEpisodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_episode);
+    }
+
+}
